8498ad57f5a242457783e7eb598b7ee028e7ff2c
RanjeetR <ranjeetr@MACPRO189.local>
2020-08-26 00:37:36 +0530

    updated for rest

--- models/GuestCollection.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const guestSchema = mongoose.Schema({
-    name: String,
-    mobile: Number,
-    dob: String,
-    doa: String
-  });
-module.exports = mongoose.model('GuestCollection',guestSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,8 @@
-const routes = require('express').Router();
-import products from './products';
-import users from './userdetails';
-import  validateData  from "./validateData";
-import tokenGeneration from './tokenGeneration';
+const routes = require("express").Router();
+import getLeader from "./getLeaders";
+import updateScore from "./updateScore";
 
-// routes.use('/products', products);
-routes.use('/save', users);
-routes.use('/validate', validateData);
-routes.use('/token', tokenGeneration);
-
-routes.use('/', (req, res) => {
-  res.status(200).json({ message: 'Connected!  CICD working  ....' });
-});
-
-routes.use('/test', (req, res) => {
-  console.log('xxx', req);
-  res.status(200).json({ message: 'Testing  ....' });
-});
+routes.use("/all", getLeader);
+routes.use("/update", updateScore);
 
 export default routes;
